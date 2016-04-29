67e3a40aefbc4a54cd2bdb0df262ab04ddfe9510
Aidan Pinkman <user@CODECLAN19.local>
2016-04-29 13:19:32 +0100

    added some code

--- specs/warehouse_picker_spec.rb
+++ specs/warehouse_picker_spec.rb
@@ -4,7 +4,7 @@ require_relative "../warehouse_picker_functions.rb"
 class TestWarehouse < Minitest::Test
 
   def test_item_at_bay
-    item = item_at_bay('b5')
+    item = item_at_bay(:b5)
     assert_equal('nail filer', item)
   end
 
