8a3f739c352b3f638443abee238ff6ea90cc6d06
Aidan Pinkman <user@CODECLAN19.home>
2016-04-30 23:12:14 +0100

    now returns multi search

--- warehouse_picker_functions.rb
+++ warehouse_picker_functions.rb
@@ -11,8 +11,6 @@ WAREHOUSE = {
     a2: "stop sign",
     a1: 'needle',
     
-    
-
     c1: 'rusty nail',
     c2: 'drill press',
     c3: 'chalk',
@@ -39,66 +37,79 @@ WAREHOUSE = {
 
 
 
-# def item_at_bay(bay)
-#   puts WAREHOUSE["bays"][bay]
-# end
 
-# def get_item_at_location(value)
 
-#   puts "What shelf location do you want to check? ("
-#   value = gets.chomp
+# print WAREHOUSE.keys[0 .. 5].length # this moves all the keys into an array [this range shows what each item is].length shown how long the area is or in this instance how far apart the items are
+# print WAREHOUSE.values[0 .. 5].length # this moves all the values into an array [this range shows what each item is].length shown how long the area is or in this instance how far apart the items are
 
-# end
 
 def intro()
    puts "Welcome, are you checking whats at a shelf location (option 1)? or are you checking to see where an item is (option  2)? select 1 or 2 to continue: "
    response = gets.chomp.to_i
     if response == 1 
-      looking_for_item
+      multi_location_search
     elsif response == 2
-      looking_for_bay
+      mu
+
     end
 end
 
-
+# #this function tells you what item is at the bay you type in, only works for one item
 def item_at_bay(location)
   return WAREHOUSE[location]
 end
 
+# #this function tells you what bay the item you tyoe in belongs in
 def location_of_item(product)
   return WAREHOUSE.key(product)
 end
 
-def multi_locations
-  puts "How many locations are you checking today?: "
+# def looking_for_item
+
+#   print "\nwhat location(s) are you interested in? (a1-a10, b1-b10 or c1-c10)?: (if unknown please press q to move on) "
+#   location = gets.chomp.to_sym
+#   print item_at_bay(location)
+
+# end
+
+# def looking_for_bay
+
+#   print "\ndo you know what item(s) you require? enter for location: "
+#   product = gets.chomp
+#   print location_of_item(product)
+# end
+
+def multi_location_search
+  loc_arr = []
+  i = 0
+  print "how many locations do you want to search?: "
+  locations = gets.chomp.to_i
+  until i >= locations 
+    print "\nwhat location(s) are you interested in? (a1-a10, b1-b10 or c1-c10)?: "
+    location = gets.chomp.to_sym
+    x = item_at_bay(location)
+    loc_arr <<  x
+    i += 1
+  end
+  print loc_arr
+end
+
+def multi_item_search
+  it_arr = []
+  i = 0
+  print "\nhow many items are you trying to find?: "
   num = gets.chomp.to_i
-  #num will equal the number of index' we want to look at once I convert the hash into an array. I must then make the index' that are searched for into an array, sort them and make sure it fully populates the area with everything inbetween so i can use .length to give the distance
-end
-
-def multi_products
-  puts "How many items do you need to find?: "
-  num = gets.chomp
-  #i will need to do the same as above but make sure i convert the values to keys before i convert it into an array. then the process should be the same
-end
-
-
-def looking_for_item
-  
-  print "\nwhat locations are you interested in? (a1-a10, b1-b10 or c1-c10)?: (if unknown please press q to move on) "
-  location = gets.chomp.to_sym
-  print item_at_bay(location)
-
-end
-
-def looking_for_bay
-
-  print "\ndo you know what item you require? enter for location: "
-  product = gets.chomp
-  print location_of_item(product)
+  until i >= num
+    print "what item are you trying to find?: "
+    product = gets.chomp
+    y = location_of_item(product)
+    it_arr << y
+    i += 1
+  end
+  print it_arr
 end
 
 
 intro
-
 
 
